073f5dbb916229891d964046421b890f41cd7172
HlDROXX <oscar15espinosa@gmail.com>
2025-09-21 23:31:08 -0500

    Version 1.1 Oscar

---
+++ cpi/AuthService.Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Domain;
+
+public class User
+{
+    public int ID { get; set; }                // PK
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Role { get; set; } = "Viewer"; // Admin | Seller | Viewer
+    public byte[]? PasswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
+}
